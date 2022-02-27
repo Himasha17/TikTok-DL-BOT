20c8f16f57592b1e26562342558f7689c7055366
Himasha Nilakshana <69343742+Himasha17@users.noreply.github.com>
2022-02-27 14:30:01 +0530

    Update main.py

--- main.py
+++ main.py
@@ -13,8 +13,7 @@ import logging
 
 
 # TikTok Downloader API
-API = 'https://api.single-developers.software/tiktok?url='
-
+API = 'https://single-developers.up.railway.app'
 # Your BOT Token
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 
