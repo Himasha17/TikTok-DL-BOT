b50414571cb0e147e4ac702c62571e3f8e2a9bae
Himasha Nilakshana <69343742+Himasha17@users.noreply.github.com>
2022-02-27 15:27:29 +0530

    Update main.py

--- main.py
+++ main.py
@@ -68,6 +68,7 @@ def Download_Video(Link,update, context):
 
 🔰 Powerd by : [🏖 TikTok Download API 🏖](https://github.com/Single-Developers/API/blob/main/tiktok/Note.md)
 [🔥 Single Developers </> ](https://t.me/SingleDevelopers) Corporation ©️
+[Meme stock](https://t.me/Meme_Stock_o_O)
 
 ◇───────────────◇"""
     
